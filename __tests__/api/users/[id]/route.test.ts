b753fce99c807cfe37f4232b4e16b2daa18be764
Zaher Ghaibeh <zaher@zaher.dev>
2024-05-07 20:23:37 +0300

    no need for node-mocks-http

--- __tests__/api/users/[id]/route.test.ts
+++ __tests__/api/users/[id]/route.test.ts
@@ -1,17 +1,17 @@
 import { expect, test } from "vitest";
-import { createRequest } from "node-mocks-http";
-
 import { GET } from "@/src/api/users/[id]/route";
+import { NextRequest } from "next/server";
 
 test("Get use by id endpoint works as expected with mocking", async () => {
-    const request = createRequest({ method: "GET", url: "/api/users/1" });
-    const response = await GET({ ...request } as any, { params: { id: '1' } });
+  const req = new NextRequest('http://doesntmatter', { method: 'GET' });
+  const response = await GET(req, {params: { id: '1' }});
 
-    expect(await response.json()).toMatchObject({
+
+  expect(await response.json()).toMatchObject({
     userId: 1,
     id: 1,
     title: 'first post title',
     body: 'first post body',
   })
-    expect(response.status).toBe(200);
+  expect(response.status).toBe(200);
 });
--- __tests__/api/users/route.test.ts
+++ __tests__/api/users/route.test.ts
@@ -2,16 +2,17 @@ import { expect, test } from "vitest";
 import { createRequest } from "node-mocks-http";
 
 import { GET } from "@/src/api/users/route";
+import { NextRequest } from "next/server";
 
 test("User endpoint works as expected with mocking", async () => {
-    const request = createRequest({ method: "GET", url: "/api/users" });
-    const response = await GET({ ...request } as any);
+  const req = new NextRequest('http://doesntmatter', { method: 'GET' });
+  const response = await GET(req);
 
-    expect(await response.json()).toMatchObject([{
+  expect(await response.json()).toMatchObject([{
     userId: 1,
     id: 1,
     title: 'first post title',
     body: 'first post body',
-  }])
-    expect(response.status).toBe(200);
+  }]);
+  expect(response.status).toBe(200);
 });
