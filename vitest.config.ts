98d3ee767effda1891f7d944dbc06747ebece400
Zaher Ghaibeh <zaher@zaher.dev>
2024-05-08 09:11:56 +0300

    update vitest config so it will load .env.test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from 'url';
+import {loadEnv} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
+    // This way we can pick the environment from `.env.test`` file and it
+    // won't need to be prefixed with `VITE_`
+    env: loadEnv('test', process.cwd(), ''),
     environment: "node",
     setupFiles: './__tests__/setup.ts',
   },
